bd07c9b205c33659e4710141b7b04cc65abb2498
mukhulishviliotari <mukhulishviliotari@gmail.com>
2026-08-07 23:53:13 +0400

    Add index.html, index.js, style.css, and images

---
+++ index.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const observerOptions = {
+    threshold: 0.15
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target); 
+      }
+    });
+  }, observerOptions);
+
+  document.querySelectorAll('.scroll-fade').forEach(el => {
+    observer.observe(el);
+  });
+});
